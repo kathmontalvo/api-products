fb4667cc5008826cd900668d14bd4ac0d2e4b814
kathm <kathmontalvo123@gmail.com>
2026-06-17 21:58:18 -0500

    fix: identado

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,105 +5,105 @@ import { ProductDTO } from './products/DTOs/product.dto';
 @Injectable()
 export class AppService {
 
-    private db: Product[] = [
-        {
-            id: '1',
-            name: 'Laptop DMC',
-            price: 10,
-            creationDate: new Date(),
-            inStock: true
-        },
-        {
-            id: '2',
-            name: 'Mouse DMC',
-            price: 5,
-            creationDate: new Date(),
-            inStock: true
-        },
-        {
-            id: '3',
-            name: 'Keyboard DMC',
-            price: 7,
-            creationDate: new Date(),
-            inStock: false
-        },
-        {
-            id: '4',
-            name: 'Monitor DMC',
-            price: 15,
-            creationDate: new Date(),
-            inStock: true
-        },
-        {
-            id: '5',
-            name: 'Headphones DMC',
-            price: 8,
-            creationDate: new Date(),
-            inStock: false
-        }
-
-    ];
-
-    /** CRUD operations:
-     * - Create: POST
-     * - Read: GET
-     * - Update: PUT
-     * - Delete: DELETE
-     */
-
-
-    // GET method: list items
-    getProducts(): Product[] {
-        return this.db;
+  private db: Product[] = [
+    {
+      id: '1',
+      name: 'Laptop DMC',
+      price: 10,
+      creationDate: new Date(),
+      inStock: true
+    },
+    {
+      id: '2',
+      name: 'Mouse DMC',
+      price: 5,
+      creationDate: new Date(),
+      inStock: true
+    },
+    {
+      id: '3',
+      name: 'Keyboard DMC',
+      price: 7,
+      creationDate: new Date(),
+      inStock: false
+    },
+    {
+      id: '4',
+      name: 'Monitor DMC',
+      price: 15,
+      creationDate: new Date(),
+      inStock: true
+    },
+    {
+      id: '5',
+      name: 'Headphones DMC',
+      price: 8,
+      creationDate: new Date(),
+      inStock: false
     }
 
-    getProductById(id: string): Product {
+  ];
 
-        const product = this.db.find(p=> p.id == id);
+  /** CRUD operations:
+   * - Create: POST
+   * - Read: GET
+   * - Update: PUT
+   * - Delete: DELETE
+   */
 
-        if(!product) {
-            throw new NotFoundException(`The product with id ${id} was not found.`)
-        }
 
-        return product;
+  // GET method: list items
+  getProducts(): Product[] {
+    return this.db;
+  }
+
+  getProductById(id: string): Product {
+
+    const product = this.db.find(p => p.id == id);
+
+    if (!product) {
+      throw new NotFoundException(`The product with id ${id} was not found.`)
     }
 
-    // POST method: create item
+    return product;
+  }
 
-    createProduct(product: ProductDTO):Product {
-        const newProduct: Product = {
-            id: Math.random().toString(),
-            name: product.name,
-            price: product.price,
-            inStock: product.inStock,
-            creationDate: new Date()
-        }
+  // POST method: create item
 
-        this.db.push(newProduct);
-        return newProduct;
+  createProduct(product: ProductDTO): Product {
+    const newProduct: Product = {
+      id: Math.random().toString(),
+      name: product.name,
+      price: product.price,
+      inStock: product.inStock,
+      creationDate: new Date()
     }
 
-    // PUT method: update item
+    this.db.push(newProduct);
+    return newProduct;
+  }
 
-    updateProduct(id: string, updatedProduct: ProductDTO) {
-        const previousProduct = this.getProductById(id);
-        const indexProduct = this.db.findIndex(p => p.id == id);
+  // PUT method: update item
 
-        this.db[indexProduct] = {
-            ...previousProduct,
-            ...updatedProduct
-        }
+  updateProduct(id: string, updatedProduct: ProductDTO) {
+    const previousProduct = this.getProductById(id);
+    const indexProduct = this.db.findIndex(p => p.id == id);
 
-        return this.db[indexProduct];
+    this.db[indexProduct] = {
+      ...previousProduct,
+      ...updatedProduct
     }
 
-    // DELETE method: delete item
-    deleteProduct(id: string) {
-        const previousProduct = this.getProductById(id);
-        this.db = this.db.filter( p => p.id !== id);
+    return this.db[indexProduct];
+  }
 
-        return this.getProducts();
-    }
+  // DELETE method: delete item
+  deleteProduct(id: string) {
+    const previousProduct = this.getProductById(id);
+    this.db = this.db.filter(p => p.id !== id);
+
+    return this.getProducts();
+  }
 
 
 }
