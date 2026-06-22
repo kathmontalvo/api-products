d9b0599fb9a2b4b183e27f6a5f7ba1e1942cc1f4
kathm <kathmontalvo123@gmail.com>
2026-06-22 17:21:55 -0500

    enablig CORS

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,14 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 3000;
 
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // Replace with your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    alowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? port);
 }
 bootstrap();
